f8294427d0203453f4c538ee6b8d85de288792f6
weshebert20 <weshebert20@gmail.com>
2017-11-06 20:53:16 -0700

    working on SQL stuff

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -2,17 +2,18 @@
 -- For all of these queries, you SHOULD NOT use an id # in a WHERE clause
 
 -- Find all fields (book and author related) for all books written by George R.R. Martin.
-
+SELECT title FROM authors JOIN books ON authors.id = books.author_id WHERE name LIKE 'George%';
 -- Find all fields (book and author related) for all books written by Milan Kundera.
-
+SELECT * FROM authors JOIN books ON authors.id = books.author_id WHERE name LIKE 'Milan%';
 -- Find all books written by an author from China or the UK.
-
+SELECT title FROM authors JOIN books ON authors.id = books.author_id WHERE nationality LIKE 'China' OR nationality LIKE 'United Kingdom%';
 -- Find out how many books Albert Camus wrote.
-
+SELECT COUNT(*) FROM books JOIN authors ON authors.id = books.author_id WHERE authors.name = 'Albert Camus';
 -- Find out how many books written before 1980 were by authors not from the US.
-
+SELECT COUNT(*) FROM books JOIN authors ON authors.id = books.author_id WHERE authors.nationality != 'United States of America' AND books.publication_date < 1980;
 -- For these last two, you should not need a JOIN.
 
 -- Find all authors whose names start with 'J'.
-
+SELECT * FROM authors WHERE name LIKE 'J%';
 -- Find all books whose titles contain 'the'.
+SELECT * FROM books WHERE title LIKE '%the%';
--- basic_queries.sql
+++ basic_queries.sql
@@ -2,30 +2,32 @@
 -- Unless otherwise stated, all queries should return all columns
 
 -- Get all information about all authors
-
+SELECT * FROM authors;
 -- Get just the name and birth year of all authors
-
+SELECT name, birth_year FROM authors;
 -- Get all authors born in the 20th centruy or later
-
+SELECT name, birth_year FROM authors WHERE birth_year > 1900;
 -- Get all authors born in the USA
-
+SELECT name, birth_year FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+SELECT * FROM books WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+SELECT * FROM books WHERE publication_date < 1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT title FROM books WHERE title LIKE '%the%';
 -- Add yourself as an author
-
+INSERT INTO authors VALUES (9, 'Wesley Hebert','United States of America', 1987);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
-
+INSERT INTO books VALUES (200, 'A New Earth','2006', 9);
+INSERT INTO books VALUES (201, 'Ready Player One','2013', 9);
 -- Update one of your books to have a new title
-
+UPDATE books SET publication_date = 2007 WHERE title = 'A New Earth';
 -- Delete your books
-
+DELETE FROM books WHERE title = 'A New Earth';
 -- Delete your author entry
+DELETE FROM authors WHERE name = 'Wesley Hebert';
